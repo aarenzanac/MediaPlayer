0b77618ea0ae0e42807aa6a4eb3bdf00a387a949
alex <aarenzana@gmail.com>
2020-11-13 07:25:49 +0100

    Desarrollo

--- app/src/main/java/com/example/mediaplayer/GuardarArchivos.kt
+++ app/src/main/java/com/example/mediaplayer/GuardarArchivos.kt
@@ -1,19 +1,24 @@
 package com.example.mediaplayer
 
+import androidx.core.net.toUri
 import com.example.mediaplayer.Constant.allMediaList
 import com.example.mediaplayer.Constant.videoExtensions
 import java.io.File
+import java.net.URI
 
 class GuardarArchivos {
-    fun cargarArchivos(directorio: File): Array<File>{
-        val listaArchivos: Array<File> = directorio.listFiles()
 
-        if(listaArchivos != null){
-            for(archivo in listaArchivos){
+    val listaURI: ArrayList<URI> = ArrayList<URI>()
+    var listaArchivos: ArrayList<File> = ArrayList<File>()
+
+    fun cargarArchivos(directorio: File): ArrayList<URI>{
+
+        if(directorio.isDirectory){
+            for(archivo in directorio.list().size){
                 if(archivo.isDirectory){
                     cargarArchivos(archivo)
                 }else{
-                    var nombre: String = archivo.name.toLowerCase()
+                    var URIArchivo: URI = listaURI.add(archivo.toUri())
                     for(extension: String in videoExtensions){
                         if(nombre.endsWith(extension)){
                             allMediaList.add(archivo)
@@ -22,7 +27,13 @@ class GuardarArchivos {
                 }
             }
         }
-        return listaArchivos
+        return listaURI
 
     }
+
+    private operator fun Int.iterator(): Iterator<File> {
+        listaArchivos.add(index)
+    }
 }
+
+
