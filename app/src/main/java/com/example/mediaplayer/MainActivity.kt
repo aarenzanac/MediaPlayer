2844c40bf113b5948f2c5173380149753fbb063d
alex <aarenzana@gmail.com>
2020-11-13 07:31:48 +0100

    Desarrollo

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -2,22 +2,27 @@ package com.example.mediaplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerView: RecyclerView
+    val permisos = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(this, permisos, 0)
+        recyclerView = findViewById(R.id.recyclerView)
 
-        //isDeviceProtectedStorage()
-        //inicializarRecycler()
+        inicializarRecycler()
 
     }
 
-    /*fun inicializarRecycler(){
+    fun inicializarRecycler(){
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
         recyclerView.setItemViewCacheSize(20)
@@ -26,6 +31,6 @@ class MainActivity : AppCompatActivity() {
         val adapter = RecyclerAdapter()
         recyclerView.adapter = adapter
 
-    }*/
+    }
 }
 
